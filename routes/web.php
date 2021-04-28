5ee66a66d1d5c0a1691b140ca55267d0a324b2c9
kalu ikechukwu e.a <swaykay.foreveralearner@gmail.com>
2021-04-28 16:08:15 +0100

    First Commit

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-
-use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +11,7 @@ use App\Models\User;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('home');
@@ -23,7 +20,7 @@ Route::get('/', function () {
 Route::get('datatable', function () {
     /**
      * Table Header | Footer columns
-    */
+     */
     $columns = [
         ['name' => 'name', 'sort' => 'name'],
         ['name' => 'email', 'sort' => 'email'],
@@ -32,38 +29,38 @@ Route::get('datatable', function () {
         ['name' => 'country', 'sort' => 'country'],
         ['name' => 'state', 'sort' => 'state'],
         ['name' => 'city', 'sort' => 'city'],
-        ['name' => 'address', 'sort' => 'address']
+        ['name' => 'address', 'sort' => 'address'],
     ];
-    
+
     /**
      * ['column', 'true - asc|false - desc'] is effective if [sort] is set to columns
      * To prevent SQL injection for this example I've whitelisted the necessary columns
      * by including them into the $fillable array
-    */
+     */
     $order_by = [$columns[0]['sort'], true];
-    
+
     /**
      * Dropdown for options for number of rows that can be fetched
-    */
+     */
     $page_options = [5, 10, 15, 25, 50, 100];
-    
+
     /**
      * Default page_options
-    */
+     */
     $fetch = $page_options[0];
-    
+
     /**
      * Sort Table
      * -----------
-     * columns | not recommended for large records exceeding 5k, 
-     * latest | speed is very good, 
+     * columns | not recommended for large records exceeding 5k,
+     * latest | speed is very good,
      * null | speed is the fastest
-    */
+     */
     $sort = 'columns';
-    
+
     /**
      * Max allowed for numbered paginator | switch to simple paginator
-    */
+     */
     $maxP = 5000;
 
     return view('datatable', [
@@ -71,25 +68,28 @@ Route::get('datatable', function () {
         'order_by' => $order_by,
         'page_options' => $page_options,
         'fetch' => $fetch,
-        'sort' => $sort, 
-        'maxP' => $maxP
+        'sort' => $sort,
+        'maxP' => $maxP,
     ]);
 })->name('datatable');
 
-/** 
- * 
- * 
- * private $common_folders = [
- *      'root' => 'INBOX',
- *      'junk' => 'INBOX.Spam',
- *      'drafts' => 'INBOX.Drafts',
- *      'sent' => 'INBOX.Sent',
- *      'trash' => 'INBOX.Trash'
- * ]
-*/
+Route::get('mail', function () {
+    /*
+     *
+     *
+     * private $common_folders = [
+     *      'root' => 'INBOX',
+     *      'junk' => 'INBOX.Spam',
+     *      'drafts' => 'INBOX.Drafts',
+     *      'sent' => 'INBOX.Sent',
+     *      'trash' => 'INBOX.Trash'
+     * ]
+     */
 
-Route::get('mail', function() {
-    return view('get-mails', ['sender' => 'info@provirtcomm.com', 'folder' => 'root']);
+    return view('get-mails', [
+        'sender' => 'info@provirtcomm.com', 
+        'folder' => 'root'
+    ]);
 })->name('get-mails');
 
 Route::prefix('sortable')->group(function () {
